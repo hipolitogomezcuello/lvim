9e6be216e3185776e647e5dfbe2f5d93384be035
Hipolito Gomez Cuello <hipolitogomezcuello@gmail.com>
2024-10-22 13:49:30 -0300

    disable cowboy, try to fix snippets

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local discipline = require("hipolito.discipline")
-discipline.cowboy()
+
+-- local discipline = require("hipolito.discipline")
+-- discipline.cowboy()
 
 local set = vim.keymap.set
 
--- Navigation
+-- Tmux
+set("n", "<leader>ts", function()
+  vim.cmd("!tmux split-window -c " .. vim.fn.expand("%:p:h"))
+end, { noremap = true, silent = true })
+
+-- Suggestions (not working)
+set({ "i", "c" }, "<M-CR>", "<C-e><CR>", { noremap = true, silent = true })
